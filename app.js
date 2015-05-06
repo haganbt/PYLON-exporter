82443100312ed60a0da0934e3bac2acb5af34f89
Ben <benhagan@gmail.com>
2015-05-05 21:09:07 -0700

    DS class.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 var DataSift = require('./lib/datasift/datasift')
     , log = require('./utils/logger')
     , config = require('config')
+    , ds = new DataSift(config.get('auth.username'), config.get('auth.api_key'))
     ;
 
-log.info('Hello from logger with config: ' + config.get('auth.username'));
+log.info('Hello from logger with DS: ' + typeof(ds));
