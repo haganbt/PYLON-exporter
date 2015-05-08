95710a2724f6bb30d7af7f4d0d3e48d021b654e2
Ben <benhagan@gmail.com>
2015-05-07 22:16:58 -0700

    Config structure for query types.

--- config/default.js
+++ config/default.js
@@ -1,12 +1,23 @@
 'use strict';
 
-var path = require('path')
-    , fs = require('fs')
-    ;
-
 module.exports = {
+    "index": {
+        "hash" : "1fdec37f185a269b57e8252bc4635853"
+    },
     "auth": {
         "username": "CS_2",
         "api_key": "8bc6d49909a9f1220b35d573acfeee90"
+    },
+    "analysis": {
+        "frequencyDistribution": [
+            {
+                "target": "fb.author.gender",
+                "threshold": 5,
+            },
+            {
+                "target": "fb.author.age",
+                "threshold": 5,
+            }
+        ]
     }
 };
