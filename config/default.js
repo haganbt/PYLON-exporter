d06d08c18a71627419263e3b857538c1d09c6182
Ben <benhagan@gmail.com>
2015-05-07 22:40:48 -0700

    Simplify hash.

--- config/default.js
+++ config/default.js
@@ -1,9 +1,7 @@
 'use strict';
 
 module.exports = {
-    "index": {
-        "hash" : "1fdec37f185a269b57e8252bc4635853"
-    },
+    "hash" : "1fdec37f185a269b57e8252bc4635853",
     "auth": {
         "username": "CS_2",
         "api_key": "8bc6d49909a9f1220b35d573acfeee90"
