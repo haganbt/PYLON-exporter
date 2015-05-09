e0e9e6e772420a79a6eeaebc275a77e9097eedf7
Ben <benhagan@gmail.com>
2015-05-08 21:03:24 -0700

    taskManager module and example fd's.

--- app.js
+++ app.js
@@ -1,46 +1,24 @@
 'use strict';
 
 var log = require('./utils/logger')
-    , config = require('config')
     , async = require('async')
-    , request = require('request')
+    , taskManager = require('./lib/taskManager')
+    , jobs = taskManager.load()
     ;
 
-var options = {
-    'auth': {
-        'user': config.get('auth.username'),
-        'pass': config.get('auth.api_key')
-    },
-    'uri': 'https://api.datasift.com/v1/pylon/get?hash='+config.get('hash')
-};
-
-
-var tasks = config.get('analysis.frequencyDistribution');
-var jobs = {};
-
-for (var prop in tasks) {
-    if (tasks.hasOwnProperty(prop)) {
-        //log.info(tasks[prop]);
-        jobs[tasks[prop].target] = function(next){
-            request(options, function (err, response, body) {
-                if (err) next(err);
-                log.info(body);
-                next(null, body);
-            });
-        };
-    }
-}
-
-console.log(jobs);
-
-
-
+/**
+ * Run the tasks array of functions in parallel, without waiting until
+ * the previous function has completed. If any of the functions pass
+ * an error to its callback, the main callback is immediately called
+ * with the value of the error. Once the tasks have completed, the
+ * results are passed to the final callback as an array.
+ */
 async.parallelLimit(jobs, 3
-, function(err, results) {
-    if(err){
-        log.error(err);
-    }
+    , function (err, results) {
+        if (err) {
+            log.info(err);
+        }
 
-    log.info(results);
-    log.info("Complete.");
-});
+        log.info(results);
+        log.info("Complete.");
+    });
