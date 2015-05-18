8d0803bf5d8fa08844f87c45379ae0621a96e85e
Ben <benhagan@gmail.com>
2015-05-18 09:59:18 -0700

    Add default targets for FD.

--- config/default.js
+++ config/default.js
@@ -11,12 +11,229 @@ module.exports = {
             {
                 "target": "fb.author.age",
                 "threshold": 5
+            },
+            {
+                "target": "fb.author.country",
+                "threshold": 5
+            },
+            {
+                "target": "fb.author.country_code",
+                "threshold": 5
+            },
+            {
+                "target": "fb.author.gender",
+                "threshold": 5
+            },
+            {
+                "target": "fb.author.region",
+                "threshold": 5
+            },
+            {
+                "target": "fb.author.type",
+                "threshold": 5
+            },
+            {
+                "target": "fb.content",
+                "threshold": 5
+            },
+            {
+                "target": "fb.hashtags",
+                "threshold": 5
+            },
+            {
+                "target": "fb.interface",
+                "threshold": 5
+            },
+            {
+                "target": "fb.language",
+                "threshold": 5
+            },
+            {
+                "target": "fb.link",
+                "threshold": 5
+            },
+            {
+                "target": "fb.media_type",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.author.age",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.author.country",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.author.country_code",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.author.gender",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.author.type",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.content",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.hashtags",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.interface",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.language",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.link",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.sentiment",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.about",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.category",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.company_overview",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.location_city",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.location_street",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.mission",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.name",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.products",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.release_date",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.username",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topics.website",
+                "threshold": 5
+            },
+            {
+                "target": "fb.parent.topic_ids",
+                "threshold": 5
+            },
+            {
+                "target": "fb.sentiment",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.about",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.category",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.company_overview",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.location_city",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.location_street",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.mission",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.name",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.products",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.release_date",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.username",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topics.website",
+                "threshold": 5
+            },
+            {
+                "target": "fb.topic_ids",
+                "threshold": 5
+            },
+            {
+                "target": "fb.type",
+                "threshold": 5
+            },
+            {
+                "target": "links.code",
+                "threshold": 5
+            },
+            {
+                "target": "links.domain",
+                "threshold": 5
+            },
+            {
+                "target": "links.normalized_url",
+                "threshold": 5
+            },
+            {
+                "target": "links.url",
+                "threshold": 5
             }
         ],
         "timeSeries": [
             {
                 "interval": "hour",
-                "span": 2
+                "span": 1
+            },
+            {
+                "interval": "day"
+            },
+            {
+                "interval": "week"
+            },
+            {
+                "interval": "month"
             }
         ]
     }
