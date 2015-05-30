ceb55dcce356399edb5caa8d38fcbb7e043f304d
Ben <benhagan@gmail.com>
2015-05-30 11:00:21 -0700

    Refactor buildFromResponse out of app.js

--- app.js
+++ app.js
@@ -13,47 +13,14 @@ oe.enqueue(configTasks, function(err, data, then){
         log.error(err);
     }
 
-    //secondaryTasks
-    if(Object.keys(then).length !== 0){
-        log.debug("Then obj: " + JSON.stringify(then));
+    log.info("::: result (primary): " + JSON.stringify(data));
 
-        var secondaryTasks = []
-            , results = data.analysis.results || {}
-            , resultTarget = data.analysis.parameters.target || ""
-            ;
 
-        log.debug("Results array: " + JSON.stringify(results));
+    if(then && Object.keys(then).length !== 0) {
 
-        //process result keys and build the secondary task obj
-        for (var ind in results) {
-            if (results.hasOwnProperty(ind)) {
+        var secondaryTasks = taskManager.buildFromResponse(data, then);
 
-                //new req params obj
-                var reqParams = taskManager.getDefaultTaskObj();
-
-                //reqParams.parameters.parameters.target = then.target;
-
-                //iterate the then params and push to the new req obj
-                for (var thenInd in then) {
-                    if (then.hasOwnProperty(thenInd)) {
-                        reqParams.json.parameters.parameters[thenInd]
-                            = then[thenInd];
-                    }
-                }
-
-                //filter - TODO hashmap for operator lookup
-                reqParams.json.filter = resultTarget
-                        + ' == "' + results[ind].key + '"';
-
-                //log.debug( "Generated secondary params: "
-                //      + JSON.stringify(reqParams));
-
-                secondaryTasks.push(reqParams);
-            }
-        }
-        //log.debug( "secondaryTasks: " + JSON.stringify(secondaryTasks));
-
-        //make secondary requests
+        //queue secondary requests
         oe.enqueue(secondaryTasks, function(sErr, sData) {
             if (sErr) {
                 log.error(sErr);
@@ -61,6 +28,4 @@ oe.enqueue(configTasks, function(err, data, then){
             log.info("::: result (secondary): " + JSON.stringify(sData));
         });
     }
-
-    log.info("::: result (primary): " + JSON.stringify(data));
 });
