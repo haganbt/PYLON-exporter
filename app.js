027f9acde50ae49462a48f76be4ea4185cbc83d8
Ben <benhagan@gmail.com>
2015-06-02 19:03:51 -0700

    Secondary tasks helper now moved to operationsEngine

--- app.js
+++ app.js
@@ -8,32 +8,12 @@ var log = require('./utils/logger')
 var oe = new OperationsEngine();
 var configTasks = taskManager.buildFromConfig();
 
-
-//TODO - seperation 1) load from config, 2) handle response to call enqueue once again
-
-
-oe.enqueue(configTasks, function(err, data, task){
+oe.process(configTasks, function(err, data, task){
     if(err){
         log.error(err);
     }
 
-    log.info("::: result (primary): " + JSON.stringify(data));
-    log.info("::: result (primary) task : " + JSON.stringify(task));
+    log.info("::: result: " + JSON.stringify(data.analysis.results));
+    log.info("::: task: " + JSON.stringify(task.json));
 
-
-    if(task.then && Object.keys(task.then).length !== 0) {
-
-        var secondaryTasks = taskManager.buildFromResponse(data, task.then);
-
-        //queue secondary requests
-        oe.enqueue(secondaryTasks, function(sErr, sData, sTask) {
-            if (sErr) {
-                log.error(sErr);
-            }
-            log.info("::: result (secondary): " + JSON.stringify(sData));
-            log.info("   ---- : " + JSON.stringify(sTask.json.parameters.analysis_type));
-            log.info("   ---- : " + JSON.stringify(sTask.json.parameters.parameters.target));
-            log.info("   ---- : " + JSON.stringify(sTask.json.filter));
-        });
-    }
 });
