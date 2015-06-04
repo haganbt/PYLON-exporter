645c3476b7ad9fa223d780539620f5dd927e39f6
samaybar <saybar@Sams-Mac-Pro.local>
2015-06-03 22:25:07 -0400

    dump data to csv and write to single file

--- app.js
+++ app.js
@@ -3,9 +3,11 @@
 var log = require('./utils/logger')
     , taskManager = require('./lib/taskManager')
     , OperationsEngine = require('./lib/OperationsEngine')
-    , json2csv = require('json2csv')
+    , fs = require('fs');
     ;
 
+var outputFileName = "output.txt";
+var dataCSV;
 var oe = new OperationsEngine();
 var configTasks = taskManager.buildFromConfig();
 
@@ -16,18 +18,29 @@ oe.process(configTasks, function(err, data, task){
         console.log("\n");
         log.info("REQUEST ::: " + JSON.stringify(task.json));
         log.info("RESPONSE ::: " + JSON.stringify(data));
+        dataCSV = "\nQuery:\n"+JSON.stringify(task.json)+"\nResults: \n"+jsonToCsv(data);
+        writeData(dataCSV);
 
-        var fields = ['key', 'interactions', 'unique_authors'];
-
-        if(data.analysis.results){
-            json2csv({ data: data.analysis.results, fields: fields }, function(err, csv) {
-                if (err) console.log(err);
-
-
-                log.info(csv);
-            });
-        }
     }
 });
 
+function jsonToCsv(data) {
+    var csv = '';
+    if (typeof (data) !== 'object' || !data.analysis.results) return;
 
+    for (var i = 0; i < data.analysis.results.length; i++) {
+        csv +=data.analysis.results[i].key + ',' + 
+            data.analysis.results[i].unique_authors + "\n";
+    }
+    return csv;
+}
+
+function writeData(analysisData){
+  fs.appendFile(outputFileName, analysisData, function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Result saved!");
+});  
+}
