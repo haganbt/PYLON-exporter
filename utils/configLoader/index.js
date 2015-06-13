0ce3d647ac2a66f2b331afe0ec1973ff7a9e125f
Ben <benhagan@gmail.com>
2015-06-13 09:28:00 -0700

    todo

--- utils/configLoader/index.js
+++ utils/configLoader/index.js
@@ -6,10 +6,18 @@ var log = require('../logger')
 var config = require('config')
     ;
 
+/**
+ * Load and validate config file
+ *
+ * @returns obj
+ */
 var load = function load() {
     try{
-        var tasks = config.get('analysis');
-        return tasks;
+        var configObj = config.get('analysis');
+
+        //todo validation
+
+        return configObj;
     } catch(e){
         log.error(new Error("Unable to process config file. Exiting."));
         process.exit(0);
