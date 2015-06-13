427a8e91b36e6f441a620dc65b7db4cbcacf8a7b
Ben <benhagan@gmail.com>
2015-06-12 23:35:09 -0700

    Async test setup

--- test/integration/freqDist.test.js
+++ test/integration/freqDist.test.js
@@ -7,33 +7,18 @@ var chai = require("chai")
 var expect = chai.expect
     ;
 
-var OperationsEngine = require('../../lib/OperationsEngine');
+var OperationsEngine = require('../../lib/OperationsEngine')
+    , taskManager = require('../../lib/taskManager')
+    ;
 
 var oe = new OperationsEngine();
 
-describe("A frequency distribution ", function() {
+describe("A frequency distribution", function() {
 
     this.timeout(10 * 60 * 1000);
 
-    it.skip("with a single target should succeed", function(){
-        var config = {
-            "app": {
-                "max_parallel_tasks": 3
-            },
-            "hash": "b5566a154bb6dcd19b52b3b431e59373",
-            "auth": {
-                "username": "CS_2",
-                "api_key": "5c15152f7086f7f3b2d2cc6b25648e94"
-            },
-             "analysis": {
-             "freqDist": [
-                 {
-                 "target": "fb.parent.author.gender",
-                 "threshold": 2
-                 }
-             ]
-             }
-        };
+    it.only("with a single target should succeed", function(done){
+        var config = taskManager.getDefaultTaskObj();
 
         oe.process(config, function(err, data, task){
             if(err){
@@ -41,6 +26,10 @@ describe("A frequency distribution ", function() {
             }
             expect(data).to.exist;
             expect(task).to.exist;
+
+            console.log(data);
+            done();
+
         });
             /*
             should.not.exist(err);
