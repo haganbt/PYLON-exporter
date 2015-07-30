85274e85e9a5f248f39310da5daf30dd9a2bc323
Ben <benhagan@gmail.com>
2015-07-30 16:13:45 -0700

    TS tests for duplicates and name property.

--- test/integration/timeSeries.test.js
+++ test/integration/timeSeries.test.js
@@ -48,8 +48,7 @@ describe("Time Series", function() {
         });
     });
 
-    it("should merge two parent requests", function(done){
-
+    it("should merge two parent requests, using the filter", function(done){
         var taskConfig = {
             "timeSeries": [
                 [
@@ -75,6 +74,41 @@ describe("Time Series", function() {
             should.not.exist(err);
             data.should.be.an('object');
             task.should.be.an('object');
+            expect(data).to.have.keys(
+                "fb.content contains \"ford\"", "fb.content contains \"honda\"");
+            done();
+        });
+    });
+
+    it.only("should merge two parent requests, using the names", function(done){
+        var taskConfig = {
+            "timeSeries": [
+                [
+                    {
+                        "name": "foo",
+                        "filter": "fb.content contains \"ford\"",
+                        "interval": "week",
+                        "span": 1
+                    },
+                    {
+                        "name": "bar",
+                        "filter": "fb.content contains \"honda\"",
+                        "interval": "week",
+                        "span": 1
+                    }
+                ]
+            ]
+        };
+        var tasks = taskManager.buildFromConfig(taskConfig);
+
+        oe.process(tasks, function(err, data, task){
+            if(err){
+                log.error(err);
+            }
+            should.not.exist(err);
+            data.should.be.an('object');
+            task.should.be.an('object');
+            expect(data).to.have.keys("foo", "bar");
             done();
         });
     });
