a95e591ee596d12bce9f4d0632ba2c6c97464b1a
Ben <benhagan@gmail.com>
2015-07-30 17:34:16 -0700

    Set default env

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 'use strict';
 
+if(process.env.NODE_ENV === undefined){
+    process.env.NODE_ENV = "test";
+}
+
 var log = require('./utils/logger')
     , taskManager = require('./lib/taskManager')
     , OperationsEngine = require('./lib/OperationsEngine')
     ;
+
+log.info("Using config file: " + process.env.NODE_ENV);
 
 var oe = new OperationsEngine();
 var configTasks = taskManager.buildFromConfig();
@@ -13,7 +19,7 @@ oe.process(configTasks, function(err, data, task){
         log.error(err);
     } else {
         console.log("\n");
-        //log.info("REQUEST ::: " + JSON.stringify(task.json));
+        log.info("REQUEST ::: " + JSON.stringify(task.json));
         log.info("RESPONSE ::: " + JSON.stringify(data));
     }
 });
