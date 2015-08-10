7650cf708cbe927fd1ce1d9b1dd4c201432afbea
Ben <benhagan@gmail.com>
2015-08-10 14:01:42 -0700

    Add test default creds.

--- config/test.js
+++ config/test.js
@@ -1,20 +1,12 @@
 "use strict";
 
 module.exports = {
-    "analysis": {
-        "freqDist": [
-            {
-                "name": "example_freqDist",
-                "target": "fb.parent.topics.name",
-                "threshold": 3
-            }
-        ],
-        "timeSeries": [
-            {
-                "name": "example_timeSeries",
-                "interval": "week",
-                "span": 2
-            }
-        ]
+    "app": {
+        "format": "csv"
+    },
+    "hash": "fd4f0cfef1807e327476ff60288bea78",
+    "auth": {
+        "username": "CS_2",
+        "api_key": "5c15152f7086f7f3b2d2cc6b25648e94"
     }
 };
