802c8995340f3f97652b9ba46faf10bc065c008d
Ben <benhagan@gmail.com>
2015-08-10 14:41:32 -0700

    Remove default index creds.

--- config/default.js
+++ config/default.js
@@ -6,10 +6,5 @@ module.exports = {
         "write_to_file": false, // true, false
         "max_parallel_tasks": 3, // number of parallel requests
         "log_level": "info" // info, warn, debug
-    },
-    "hash": "fd4f0cfef1807e327476ff60288bea78",
-    "auth": {
-        "username": "CS_2",
-        "api_key": "5c15152f7086f7f3b2d2cc6b25648e94"
     }
 };
