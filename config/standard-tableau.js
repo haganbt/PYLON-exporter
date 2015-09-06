89d89f26c3c74ee13cb9bd0f41bde72b7a7c1015
Ben <benhagan@gmail.com>
2015-09-06 14:48:01 -0700

    Add tableau dashboard for daily recording activity #57

--- config/standard-tableau.js
+++ config/standard-tableau.js
@@ -11,127 +11,170 @@ module.exports = {
     },
     "analysis": {
         "freqDist": [
-        /**
-         * Media types by entity
-         */
-            {
-                "name": "media_types",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "child": {
-                    "target": "fb.media_type",
-                    "threshold": 6
-                }
-            },
-        /**
-         * Type by entity
-         */
-            {
-                "name": "type",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "child": {
-                    "target": "fb.type",
-                    "threshold": 6
-                }
-            },
-        /**
-         * Total Entity Volumes
-         */
-            {
-                "name": "entity_volumes",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 5
-            },
-        /**
-         * Age Gender tornadoes
-         */
-            {
-                "name": "age_gender",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "child": {
-                    "target": "fb.author.age",
+            /**
+             * Media types by entity
+             */
+                {
+                    "name": "media_types",
+                    "target": "interaction.tag_tree.standard",
                     "threshold": 6,
                     "child": {
-                        "target": "fb.author.gender",
-                        "threshold": 2
+                        "target": "fb.media_type",
+                        "threshold": 6
+                    }
+                },
+            /**
+             * Type by entity
+             */
+                {
+                    "name": "type",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 6,
+                    "child": {
+                        "target": "fb.type",
+                        "threshold": 6
+                    }
+                },
+            /**
+             * Total Entity Volumes
+             */
+                {
+                    "name": "entity_volumes",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 5
+                },
+            /**
+             * Age Gender tornadoes
+             */
+                {
+                    "name": "age_gender",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 6,
+                    "child": {
+                        "target": "fb.author.age",
+                        "threshold": 6,
+                        "child": {
+                            "target": "fb.author.gender",
+                            "threshold": 2
+                        }
+                    }
+                },
+            /**
+             * Entity volume by region
+             */
+                {
+                    "name": "region_by_entity",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 6,
+                    "then": {
+                        "target": "fb.author.region",
+                        "threshold": 20
+                    }
+                },
+            /**
+             * URLs and domains
+             */
+                {
+                    "name": "domains_by_entity",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 6,
+                    "then": {
+                        "filter": "not links.domain in \"bit.ly, bitly.com, facebook.com\"",
+                        "target": "links.domain",
+                        "threshold": 25
+                    }
+                },
+                {
+                    "name": "links_by_entity",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 6,
+                    "then": {
+                        "filter": "not links.domain in \"bit.ly, bitly.com, facebook.com\"",
+                        "target": "links.url",
+                        "threshold": 25
+                    }
+                },
+            /**
+             * Entity topics
+             */
+                {
+                    "name": "topics_by_entity",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 3,
+                    "then": {
+                        "target": "fb.topics.name",
+                        "threshold": 100
+                    }
+                },
+            /**
+             * Topic hashtags
+             */
+                {
+                    "name": "topic_hashtags",
+                    "target": "fb.topics.name",
+                    "threshold": 200,
+                    "then": {
+                        "target": "fb.hashtags",
+                        "threshold": 50
+                    }
+                },
+            /**
+             * Entity timeSeries
+             */
+                {
+                    "name": "timeSeries_by_entity",
+                    "target": "interaction.tag_tree.standard",
+                    "threshold": 30,
+                    "then": {
+                        "type": "timeSeries",
+                        "interval": "day"
                     }
                 }
-            },
-        /**
-         * Entity volume by region
-         */
-            {
-                "name": "region_by_entity",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "then": {
-                    "target": "fb.author.region",
-                    "threshold": 20
-                }
-            },
-        /**
-         * URLs and domains
-         */
-            {
-                "name": "domains_by_entity",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "then": {
-                    "filter": "not links.domain in \"bit.ly, bitly.com, facebook.com\"",
-                    "target": "links.domain",
-                    "threshold": 25
-                }
-            },
-            {
-                "name": "links_by_entity",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 6,
-                "then": {
-                    "filter": "not links.domain in \"bit.ly, bitly.com, facebook.com\"",
-                    "target": "links.url",
-                    "threshold": 25
-                }
-            },
-        /**
-         * Entity topics
-         */
-            {
-                "name": "topics_by_entity",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 3,
-                "then": {
-                    "target": "fb.topics.name",
-                    "threshold": 100
-                }
-            },
-        /**
-         * Topic hashtags
-         */
-            {
-                "name": "topic_hashtags",
-                "target": "fb.topics.name",
-                "threshold": 200,
-                "then": {
-                    "target": "fb.hashtags",
-                    "threshold": 50
-                }
-            },
-        /**
-         * Entity timeSeries
-         */
-            {
-                "name": "timeSeries_by_entity",
-                "target": "interaction.tag_tree.standard",
-                "threshold": 30,
-                "then": {
-                    "type": "timeSeries",
-                    "interval": "day"
-                }
-            }
         ],
         "timeSeries": [
+            /**
+             * Daily Volumes
+             */
+                {
+                    "name": "timeSeries_by_day",
+                    "interval": "day"
+                },
+            /**
+             * Topics by day
+             */
+                {
+                    "name": "topics_by_day",
+                    "interval": "day",
+                    "then": {
+                        "type": "freqDist",
+                        "target": "fb.topics.name",
+                        "threshold": 100
+                    }
+                },
+            /**
+             * Hashtags by day
+             */
+                {
+                    "name": "hashtags_by_day",
+                    "interval": "day",
+                    "then": {
+                        "type": "freqDist",
+                        "target": "fb.hashtags",
+                        "threshold": 100
+                    }
+                },
+            /**
+             * Links by day
+             */
+                {
+                    "name": "links_by_day",
+                    "interval": "day",
+                    "then": {
+                        "type": "freqDist",
+                        "target": "links.url",
+                        "threshold": 100
+                    }
+                }
         ]
     }
 };
