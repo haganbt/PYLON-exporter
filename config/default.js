631dbb28f9f908ed37dc016e378d1d763ad7ea6d
Ben <benhagan@gmail.com>
2015-10-19 10:43:22 -0700

    Update default parallel tasks to 8

--- config/default.js
+++ config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     "app": {
         "format": "json", // json, csv
         "write_to_file": false, // true, false
-        "max_parallel_tasks": 3, // number of parallel requests
+        "max_parallel_tasks": 8, // number of parallel requests
         "log_level": "info" // info, warn, debug
     }
 };
